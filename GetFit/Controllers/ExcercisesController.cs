a1fb74e9b91be42ad8efcd1d3f8391b2009c5bef
Palmeuh <fredrik.palme@utb.ecutbildning.se>
2021-07-13 13:29:10 +0200

    Add project files.

---
+++ GetFit/Controllers/ExcercisesController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GetFit.Domain.Models;
+using GetFit.Infrastructure;
+
+
+namespace GetFit.Controllers
+{
+    public class ExcercisesController : Controller
+    {
+        private readonly GetFitContext _context;
+
+        public ExcercisesController(GetFitContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Excercises
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Excercise.ToListAsync());
+        }
+
+        // GET: Excercises/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var excercise = await _context.Excercise
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (excercise == null)
+            {
+                return NotFound();
+            }
+
+            return View(excercise);
+        }
+
+        // GET: Excercises/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Excercises/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,MuscleGroup,Description")] Excercise excercise)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(excercise);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(excercise);
+        }
+
+        // GET: Excercises/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var excercise = await _context.Excercise.FindAsync(id);
+            if (excercise == null)
+            {
+                return NotFound();
+            }
+            return View(excercise);
+        }
+
+        // POST: Excercises/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,MuscleGroup,Description")] Excercise excercise)
+        {
+            if (id != excercise.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(excercise);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ExcerciseExists(excercise.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(excercise);
+        }
+
+        // GET: Excercises/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var excercise = await _context.Excercise
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (excercise == null)
+            {
+                return NotFound();
+            }
+
+            return View(excercise);
+        }
+
+        // POST: Excercises/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var excercise = await _context.Excercise.FindAsync(id);
+            _context.Excercise.Remove(excercise);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ExcerciseExists(int id)
+        {
+            return _context.Excercise.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ GetFit/Infrastructure/GetFitContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GetFit.Domain.Models;
+
+namespace GetFit.Infrastructure
+{
+    public class GetFitContext : DbContext
+    {
+        public GetFitContext (DbContextOptions<GetFitContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GetFit.Domain.Models.Excercise> Excercise { get; set; }
+    }
+}
