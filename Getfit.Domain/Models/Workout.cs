4aefe4d7805f642254e90833303d8a1c9bbb303f
Palmeuh <fredrik.palme@utb.ecutbildning.se>
2021-07-27 09:07:24 +0200

    Validation added to creating workouts

--- Getfit.Domain/Models/Workout.cs
+++ Getfit.Domain/Models/Workout.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace GetFit.Domain.Models
 {
     public class Workout
     {
         public int Id { get; set; }
+        [StringLength(70, MinimumLength = 2)]
+        [Required]
         public string Name { get; set; }
+        [StringLength(130, MinimumLength = 5)]
+        [Required]
         public string Description { get; set; }
         public List<Excercise> Excercises { get; set; }
         public List<WorkoutProgram> WorkoutPrograms { get; set; }
