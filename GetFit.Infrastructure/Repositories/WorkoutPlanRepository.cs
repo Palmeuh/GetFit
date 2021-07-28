387e7e96e4aa26126fc7f903172bac668be18902
Palmeuh <fredrik.palme@utb.ecutbildning.se>
2021-07-28 12:55:19 +0200

    update

---
+++ GetFit.Infrastructure/Repositories/WorkoutPlanRepository.cs
@@ -0,0 +1,29 @@
+﻿using GetFit.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetFit.Infrastructure.Repositories
+{
+    public class WorkoutPlanRepository : GenericRepository<WorkoutPlan>
+    {
+        public WorkoutPlanRepository(GetFitContext context) : base(context)
+        {
+
+        }
+
+        public override async Task<WorkoutPlan> GetById(int? id)
+        {
+            return await _context.WorkoutPlans
+                .Where(wp => wp.Id == id)
+                .Include(wpr => wpr.WorkoutProgram)
+                .ThenInclude(w => w.Workouts)
+                .ThenInclude(e => e.Excercises)
+                .FirstOrDefaultAsync();
+        }
+
+    }
+}
